8ed9c3c6baef6d15bb999b22f2ffec1618e1e7d9
yhkim <ky45042@naver.com>
2022-10-20 16:10:36 +0900

    게시판 타입필드 추가

--- main/models.py
+++ main/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
+    class BoardType(models.TextChoices):
+        FREE = 'FR', '자유게시판'
+        REVIEW = 'RV', '여행후기'
+
     subject = models.CharField(max_length=200)
+    board_type = models.CharField(max_length=2, choices=BoardType.choices, default=BoardType.FREE)
     content = models.TextField()
     create_date = models.DateTimeField()
 
