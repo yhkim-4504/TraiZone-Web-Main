59074bc211a65350a2a086a18e4177f3425f1afa
yhkim <ky45042@naver.com>
2022-10-27 15:52:01 +0900

    change static path

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Article, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 
-admin.site.register(Article)
+class ArticleAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment)
